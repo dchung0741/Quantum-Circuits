072b0e94030840e8974a3459c429e489d1165968
Daniel <30490924+dchung0741@users.noreply.github.com>
2023-02-20 08:29:19 +0800

    Fix __init__.py

---
+++ lib/Chemistry/__init__.py
@@ -0,0 +1,1 @@
+from .HartreeFock import *
--- lib/__init__.py
+++ lib/__init__.py
@@ -2,6 +2,5 @@ from .Operator_and_State import *
 from .PauliString import *
 from .FermionicOperator import *
 from .QuantumCircuit import *
-from typing_extensions import TypeAlias
-from typing import Type, Union
+from .chemistry.HartreeFock import *
 
